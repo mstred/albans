5861369e4bb78742e961b0edb502ab9ffd29ad81
Felipe Nipo <fnf@cesar.org.br>
2014-10-27 15:05:56 -0300

    Implementing with ExpressJs

--- program.js
+++ program.js
@@ -1,41 +1,17 @@
 var htmlBuilder = require("./htmlBuilder.js");
-var http = require("http");
-var server = http.createServer();
+var express = require('express');
+var server = express();
 
-server.addListener("request", function (request, response) {
+server.get("/", function (request, response) {
 	console.log("request event");
-	
+
 	var htmlContent = htmlBuilder.buildHtmlContent("Hello World");
 	response.writeHead(200, {
-		"Content-Length": htmlContent.length,
-  		"Content-Type": "text/html"
+	    "Content-Length": htmlContent.length,
+	    "Content-Type": "text/html"
 	});
 	response.write(htmlContent, "utf8");
 	response.end();
-});
-
-server.addListener("connection", function (socket) {
-	console.log("connection event");
-});
-
-server.addListener("close", function (socket) {
-	console.log("close event");
-});
-
-server.addListener("checkContinue", function (request, response) {
-	console.log("checkContinue event");
-});
-
-server.addListener("connect", function (request, socket, head) {
-	console.log("connect event");
-});
-
-server.addListener("upgrade", function (request, socket, head) {
-	console.log("upgrade event");
-});
-
-server.addListener("clientError", function (exception, socket) {
-	console.log("clientError event");
 });
 
 server.listen(1337);
