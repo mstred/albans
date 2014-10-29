4cc74016d043c79165069b2b56737f486973a52f
Felipe Nipo <fpnipo@gmail.com>
2014-10-29 08:34:53 -0200

    Adding routes

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/web/index');
+var users = require('./routes/api/users');
+var topics = require('./routes/api/topics');
+var posts = require('./routes/api/posts');
 
 var app = express();
 
@@ -23,7 +25,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/api', users);
+app.use('/api', topics);
+app.use('/api', posts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
